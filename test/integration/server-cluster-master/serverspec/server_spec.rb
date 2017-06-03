cc64f80c5b1054a126ee4e745f24a2e9f881b056
rarsan <roy.arsan@gmail.com>
2017-06-02 19:16:19 -0500

    Fix cluster master integration test

--- test/integration/server-cluster-master/serverspec/server_spec.rb
+++ test/integration/server-cluster-master/serverspec/server_spec.rb
@@ -1,13 +1,13 @@
 require 'spec_helper'
 
-describe 'chef-splunk::server should run as "root" user' do
+describe 'chef-splunk::server should run as "splunk" user' do
   describe command('ps aux | grep "splunkd -p" | head -1 | awk \'{print $1}\'') do
-    its(:stdout) { should match(/root/) }
+    its(:stdout) { should match(/splunk/) }
   end
 end
 
-describe 'chef-splunk::server should listen on web_port 443' do
-  describe port(443) do
+describe 'chef-splunk::server should listen on web_port 8443' do
+  describe port(8443) do
     it { should be_listening.with('tcp') }
   end
 end
